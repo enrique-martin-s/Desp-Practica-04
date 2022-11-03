18941641dcb6331ecb794e33e2ef3ad0cefc83de
Enrique Martin <emarsan708@g.educaand.es>
2022-11-03 11:40:43 +0000

    Añadido phpmyadmin y adminer

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,28 +3,34 @@
 # para mostrar los comandos que se van ejecutando
 set -x
 
-# nos movemos al directorio temporal para que funcione en todas las distribuciones linux
-cd /tmp
-
+# Paso 1: instalacion de phpmyadmin
 # descargamos el codigo fuente de phmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.zip --output-document /tmp/pma.zip
 
 # instalamos unzip
 apt install unzip -y
 
-# descomprimimos el archivo
-unzip /tmp/pma.zip
+# descomprimimos el archivo en /var/www/html
+unzip /tmp/pma.zip -d /var/www/html
 
-# movemos el codigo fuente de phpmyadmin
-mv /tmp/phpMyAdmin-5.2.0-all-languages /var/www/html/phpmyadmin
+# renombramos el nombre del directorio
+mv /var/www/html/phpMyAdmin-5.2.0-all-languages /var/www/html/phpmyadmin -y
 
 # borramos los temporales
-rm - f /tmp/pma.zip
+rm -f /tmp/pma.zip
+
+# borramos la base de datos de phpmyadmin de instalaciones previas
+mysql -u root <<< "DROP DATABASE IF EXISTS phpmyadmin;"
 
 # importamos la base de datos
 mysql -u root < /var/www/html/phpmyadmin/sql/create_tables.sql 
 
-# 
-cd ../sql
-mysql -u root < create_user.sql
+# Creamos el usuario de la base de datos phpmyadmin
+mysql -u root < ../sql/create_user.sql
 
+
+# Paso 2: instalacion de adminer
+# creamos un directorio para adminer
+mkdir -p /var/www/html/adminer
+# descargamos el codigo fuente
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php --output-document /var/www/html/adminer/index.php
--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,3 +1,3 @@
-Create USER pma_user@'%' IDENTIFIED BY 'pma_password';
+Create USER IF NOT EXISTS pma_user@'%' IDENTIFIED BY 'pma_password';
 GRANT ALL PRIVILEGES ON phpmyadmin.* TO pma_user@'%';
 FLUSH PRIVILEGES;
